a4c33f12bd1343e8345595e4cf06ebfae9d5199f
Shubhashis Roy <shubhashisroy360@gmail.com>
2024-07-03 22:05:04 +0530

    update drizzle config file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,16 @@
 import type { Config } from "drizzle-kit";
-import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
 if (!process.env.DATABASE_URL) {
-  console.log("Can't find database url");
+  console.log("🔴 Cannot find database url");
 }
 
-export default defineConfig({
+export default {
   schema: "./src/lib/supabase/schema.ts",
   out: "./migrations",
   driver: "pg",
   dbCredentials: {
-    connectionSring: process.env.DATABASE_URL || "",
+    connectionString: process.env.DATABASE_URL || "",
   },
-} satisfies Config);
+} satisfies Config;
